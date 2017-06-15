d3f106f605625feb0327f9afea91d1daa80681a8
Emmanuel Nanadoum <nanadoum.emmanuel@gmail.com>
2017-06-15 04:56:04 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// script.js
+
+$(function () {
+
+
+
+    // ACCORDION
+    // https://jqueryui.com/accordion/
+
+    $('#accordion').accordion({
+        collapsible: true,
+        heightStyle: "content"
+    });
+});
